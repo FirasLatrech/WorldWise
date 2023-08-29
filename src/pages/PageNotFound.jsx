eadbaf5170da0fc87bef9e36a915754e3b1459da
pkthunder87 <pkthunder87@gmail.com>
2023-08-29 13:44:14 -0700

    Lesson 207: Linking Between Routes With <Link /> and <NavLink />

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,11 @@
+import PageNav from "../components/PageNav";
+
 function PageNotFound() {
-  return <div>Not Found :(</div>;
+  return (
+    <div>
+      <PageNav /> Not Found :(
+    </div>
+  );
 }
 
 export default PageNotFound;
