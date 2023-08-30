f0766ce302c6d5796c724d489e59c2783e3ca85a
pkthunder87 <pkthunder87@gmail.com>
2023-08-30 15:50:36 -0700

    Lesson 218: Programmatic Navigation with useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 function App() {
   const [cities, setCities] = useState([]);
@@ -54,7 +55,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
